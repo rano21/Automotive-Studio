a9756478076a12ce7d7b7f0790779dd788a57700
Rania <f200310@cfd.nu.edu.pk>
2023-03-22 03:58:37 +0500

    Bike Model

---
+++ models/bikes.models.js
@@ -0,0 +1,30 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../config")
+
+const Bikes = sequelize.define("bikes", {
+    bikeName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    companyName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    model: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+
+ });
+
+module.exports = Bikes
+
+
